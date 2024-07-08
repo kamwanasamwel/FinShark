0e6ad4c0e668e395adce87ff43a87e3b4ac5eb68
kamwanasamwel <kamwanasamwel@gmail.com>
2024-07-08 09:39:59 +0300

    stock filtering by company name and symbol

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.models;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -18,9 +19,20 @@ namespace api.Repository
         {
             _context = context;
         }
-        public async Task<List<Stock>> GetAllAsync()
+        public async Task<List<Stock>> GetAllAsync(QueryObject query)
         {
-            return await _context.Stocks.Include(x => x.Comments).ToListAsync();
+            var stocks = _context.Stocks.AsQueryable();
+            if(!string.IsNullOrWhiteSpace(query.CompanyName))
+            {
+                stocks = stocks.Where(x => x.CompanyName.Contains(query.CompanyName));
+            }
+
+            if(!string.IsNullOrWhiteSpace(query.Symbol))
+            {
+                stocks = stocks.Where(x => x.Symbol.Contains(query.Symbol));
+            }
+
+            return await stocks.ToListAsync();
         }
 
         public async Task<Stock> CreateAsync(Stock stockModel)
